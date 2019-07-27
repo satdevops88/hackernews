602714cd6dba183bffcd5325d9431b5354bb5f7e
satdevops88 <sat.devops88@gmail.com>
2019-07-27 22:05:41 +0800

    Create Mutation function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import '../styles/App.css';
+import React from 'react'
+// import LinkList from './LinkList'
+import CreateLink from './CreateLink';
 
 function App() {
   return (
-    <div className="App">
-      Hacker News Application
-    </div>
+    <CreateLink />
   );
 }
 
